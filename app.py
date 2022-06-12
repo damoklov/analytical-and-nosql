907ba871475150923cec0807d4237e783c55cd79
damoklov <mishanya@protonmail.com>
2022-06-12 19:51:56 +0300

    Added basic Flask app

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+import requests
+import os
+
+from strategies.console_strategy import ConsoleStrategy
+from strategies.kafka_strategy import ConcreteStrategyB
+from strategies.utils import Context
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def load_file():
+    os.environ["CHOICE"] = "CONSOLE"
+    endpoint = request.args.get("path")
+    if not endpoint:
+        return "Provide path"
+    r = requests.get(endpoint)
+    print(os.environ["CHOICE"])
+    if os.environ["CHOICE"] == 'CONSOLE':
+        context = Context(ConsoleStrategy())
+        print("Client: Strategy is set to console output.")
+        context.do_some_business_logic(r.json())
+    elif os.environ["CHOICE"] == 'CLOUD':
+        context = Context(ConcreteStrategyB())
+        print("Client: Strategy is set to cloud output.")
+        context.do_some_business_logic(r.json())
+    else:
+        print("Something went wrong")
+        return "NOT OK"
+    return "OK"
+
+
+if __name__ == '__main__':
+    app.run()
